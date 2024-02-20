9efe615b888eb2ca3801a270e2c0ebd81f687177
bukhanovskayamarina22 <bukhanovskayamarina22@protonmail.com>
2024-02-20 21:09:25 +0300

    created input widget for the chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -37,11 +37,7 @@ class ChatPageAppBar extends StatelessWidget {
                   child: SizedBox(
                       height: 36,
                       width: 36,
-                      child: Icon(
-                        Icons.arrow_back_ios,
-                        size: 24,
-                        color: Theme.of(context).colorScheme.onSecondary,
-                      )),
+                      child: IconWithSize24(icon: Icons.arrow_back_ios, color: Theme.of(context).colorScheme.onSecondary,)),
                 ),
               ),
               Flexible(
@@ -54,6 +50,79 @@ class ChatPageAppBar extends StatelessWidget {
         ),
         const TetriaryDivider(),
       ],
+    );
+  }
+}
+
+class IconWithSize24 extends StatelessWidget {
+  final Object icon;
+  final Color color;
+  const IconWithSize24({
+    super.key, required this.icon, required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      const ImageIcon(AssetImage("assets/attach.png")) as IconData,
+      size: 24,
+      color: color
+    );
+  }
+}
+
+class InputMessage extends StatelessWidget {
+  const InputMessage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, top: 14, right: 20, bottom: 44),
+      child: Row(
+        children: [
+          const ChatPageImageIcon(path: 'assets/Attach.png',),
+          const Padding(padding: EdgeInsets.only(left: 8)),
+          Flexible(
+              child: SecondaryContainer(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: TextField(
+                textAlignVertical: TextAlignVertical.center,
+                maxLines: 99,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Сообщение",
+                    hintStyle: Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(color: Theme.of(context).colorScheme.tertiary)),
+                    
+              ),
+            ),
+          )),
+          const Padding(padding: EdgeInsets.only(left: 8)),
+          const ChatPageImageIcon(path: 'assets/Audio.png',),
+          const Padding(padding: EdgeInsets.only(left: 8)),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatPageImageIcon extends StatelessWidget {
+  final String path;
+  const ChatPageImageIcon({
+    super.key, required this.path,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SecondaryContainer(
+      width: 42,
+      child: ImageIcon(AssetImage(path),
+          size: 24, color: Theme.of(context).colorScheme.onSecondary),
     );
   }
 }
